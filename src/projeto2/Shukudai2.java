8c46f8e27d9d596d71474a75c0e0bb937382e900
marioags87 <marioandersongs@gmail.com>
2023-11-09 19:06:53 -0300

    Atualizado

---
+++ src/projeto2/Shukudai2.java
@@ -0,0 +1,30 @@
+package projeto2;
+
+import java.util.Scanner;
+
+public class Shukudai2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int Funcionario, Horas; 
+		double Valor;
+		
+		System.out.println("Digite o número do funcionário");
+		Funcionario = sc.nextInt();
+		System.out.println("Digite a quantidade de horas trabalhadas");
+		Horas = sc.nextInt();
+		System.out.println("Digite o valor da hora trabalhada");
+		Valor = sc.nextDouble();
+				
+		sc.close();
+		
+		double salario = (Horas * Valor);
+		
+		System.out.println("Número do Funcionário = " + Funcionario);
+		System.out.printf("Salário = R$ %.2f", + salario);
+
+	}
+
+}
