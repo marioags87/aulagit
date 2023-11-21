4cdfa65cc6c0fe9768d77848a11fea12fd2a0f9e
marioags87 <marioandersongs@gmail.com>
2023-11-21 19:42:49 -0300

    mudei o vizu

--- src/projeto4/Shukudai15.java
+++ src/projeto4/Shukudai15.java
@@ -4,31 +4,31 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Shukudai15 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        // Gerar um número aleatório entre 1 e 100
-        Random random = new Random();
-        int numeroAleatorio = random.nextInt(100) + 1;
-        int palpite;
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        // Loop até o usuário acertar o número
-        do {
-            // Solicitar ao usuário que faça um palpite
-            System.out.println("Adivinhe o número (entre 1 e 100):");
-            palpite = scanner.nextInt();
+    // Gerar um número aleatório entre 1 e 100
+    Random random = new Random();
+    int numeroAleatorio = random.nextInt(100) + 1;
+    int palpite;
 
-            // Verificar se o palpite é maior, menor ou igual ao número aleatório
-            if (palpite > numeroAleatorio) {
-                System.out.println("MUITO ALTO. TENTE NOVAMENTE.");
-            } else if (palpite < numeroAleatorio) {
-                System.out.println("MUITO BAIXO. TENTE NOVAMENTE.");
-            } else {
-                System.out.println("ACERTOU MIZERÁVI!");
-            }
+    // Loop até o usuário acertar o número
+    do {
+      // Solicitar ao usuário que faça um palpite
+      System.out.println("Adivinhe o número (entre 1 e 100):");
+      palpite = scanner.nextInt();
 
-        } while (palpite != numeroAleatorio);
+      // Verificar se o palpite é maior, menor ou igual ao número aleatório
+      if (palpite > numeroAleatorio) {
+        System.out.println("MUITO ALTO. TENTE NOVAMENTE.");
+      } else if (palpite < numeroAleatorio) {
+        System.out.println("MUITO BAIXO. TENTE NOVAMENTE.");
+      } else {
+        System.out.println("ACERTOU MIZERÁVI!");
+      }
+    } while (palpite != numeroAleatorio);
 
-        scanner.close();
-    }
+    scanner.close();
+  }
 }
