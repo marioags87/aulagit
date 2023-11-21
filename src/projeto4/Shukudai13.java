90e5964fffb53b153d54eabfa7bc97240c5fe85c
marioags87 <marioandersongs@gmail.com>
2023-11-21 20:44:34 -0300

    atualizei

--- src/projeto4/Shukudai13.java
+++ src/projeto4/Shukudai13.java
@@ -3,29 +3,25 @@ package projeto4;
 import java.util.Scanner;
 
 public class Shukudai13 {
-    public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 
-		int senha;
-		
-        //Estrutura de repetição OBS: Se a variavel while estiver no fim do código não precisa atribuir valor para senha
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-        do {
-        
-        System.out.println("Digite a senha:");
-		senha = sc.nextInt();
-        
-        if (senha == 2002) {
-            System.out.println("Acesso Permitido");
-        }
-        else {
-            System.out.println("Acesso Negado");
-        }
-                          
-        }
-        while (!(senha == 2002));
-		
-        sc.close();
-    }
-    
+    int senha;
+
+    //Estrutura de repetição OBS: Se a variavel while estiver no fim do código não precisa atribuir valor para senha
+
+    do {
+      System.out.println("Digite uma senha de 4 digitos:");
+      senha = sc.nextInt();
+
+      if (senha == 2002) {
+        System.out.println("Acesso Permitido");
+      } else {
+        System.out.println("Acesso Negado");
+      }
+    } while (!(senha == 2002));
+
+    sc.close();
+  }
 }
