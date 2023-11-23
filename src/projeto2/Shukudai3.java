6180a5fd7b9d942eb87d0a168f5b3e071d096eb5
marioags87 <marioandersongs@gmail.com>
2023-11-23 19:21:21 -0300

    arumei

--- src/projeto2/Shukudai3.java
+++ src/projeto2/Shukudai3.java
@@ -4,34 +4,30 @@ import java.util.Scanner;
 
 public class Shukudai3 {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int CodP1, QtP1; 
-		double ValorUniP1;
-		int CodP2, QtP2;
-		double ValorUniP2;
-		
-		System.out.println("Digite o código da primeira peça");
-		CodP1 = sc.nextInt();
-		System.out.println("Digite a quantidade de unidades");
-		QtP1 = sc.nextInt();
-		System.out.println("Digite o valor unitário por peça");
-		ValorUniP1 = sc.nextDouble();
-		System.out.println("Digite o código da segunda peça");
-		CodP2 = sc.nextInt();
-		System.out.println("Digite a quantidade de unidades");
-		QtP2 = sc.nextInt();
-		System.out.println("Digite o valor unitário por peça");
-		ValorUniP2 = sc.nextDouble();
-				
-		sc.close();
-		
-		double Total = (QtP1 * ValorUniP1 + QtP2 * ValorUniP2);
-		
-		System.out.printf("Valor Total a Pagar = R$ %.2f", Total);
-		
-	}
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    int CodP1, QtP1;
+    double ValorUniP1;
+    int CodP2, QtP2;
+    double ValorUniP2;
+
+    System.out.println("Digite o código da primeira peça");
+    CodP1 = sc.nextInt();
+    System.out.println("Digite a quantidade de unidades");
+    QtP1 = sc.nextInt();
+    System.out.println("Digite o valor unitário por peça");
+    ValorUniP1 = sc.nextDouble();
+    System.out.println("Digite o código da segunda peça");
+    CodP2 = sc.nextInt();
+    System.out.println("Digite a quantidade de unidades");
+    QtP2 = sc.nextInt();
+    System.out.println("Digite o valor unitário por peça");
+    ValorUniP2 = sc.nextDouble();
+
+    sc.close();
+
+    double Total = (QtP1 * ValorUniP1 + QtP2 * ValorUniP2);
+    System.out.printf("Valor Total a Pagar = R$ %.2f", Total);
+  }
 }
--- src/projeto4/Jokenpo.java
+++ src/projeto4/Jokenpo.java
@@ -10,54 +10,68 @@ public class Jokenpo {
     Scanner sc = new Scanner(System.in);
 
     //Layout inicial do jogo
+    int jogPts = 0;
+    int compPts = 0;
 
-    System.out.println("     Jo Ken Pô     ");
-    System.out.println("Digite 1 para PEDRA");
-    System.out.println("Digite 2 para TESOURA");
-    System.out.println("Digite 3 para PAPEL");
-    System.out.println("Escolha a sua opção:");
+    while (jogPts < 5 && compPts < 5) {
+      System.out.println("     Jo Ken Pô     ");
+      System.out.println("Digite 1 para PEDRA");
+      System.out.println("Digite 2 para TESOURA");
+      System.out.println("Digite 3 para PAPEL");
+      System.out.println("Escolha a sua opção:");
 
-    //Lógica do jogador
-    jogador = sc.nextInt();
-    switch (jogador) {
-      case 1:
-        System.out.println("Jogador escolheu PEDRA");
-        break;
-      case 2:
-        System.out.println("Jogador escolheu TESOURA");
-        break;
-      case 3:
-        System.out.println("Jogador escolheu PAPEL");
-        break;
-      default:
-        System.out.println("Opção inválida, digite novamente.");
-        break;
+      //Lógica do jogador
+      jogador = sc.nextInt();
+      switch (jogador) {
+        case 1:
+          System.out.println("Jogador escolheu PEDRA");
+          break;
+        case 2:
+          System.out.println("Jogador escolheu TESOURA");
+          break;
+        case 3:
+          System.out.println("Jogador escolheu PAPEL");
+          break;
+        default:
+          System.out.println("Opção inválida, digite novamente.");
+      }
+      //lógica do computador
+      computador = (int) (Math.random() * 3) + 1;
+
+      switch (computador) {
+        case 1:
+          System.out.println("Computador escolheu PEDRA");
+          break;
+        case 2:
+          System.out.println("Computador escolheu TESOURA");
+          break;
+        case 3:
+          System.out.println("Computador escolheu PAPEL");
+          break;
+      }
+      //Lógica para determinar o vencedor
+      if (jogador == computador) {
+        System.out.println("EMPATE");
+      } else if (
+        (jogador == 1 && computador == 2) ||
+        (jogador == 2 && computador == 3) ||
+        (jogador == 3 && computador == 1)
+      ) {
+        System.out.println("JOGADOR VENCEU.");
+        jogPts++;
+      } else {
+        System.out.println("COMPUTADOR VENCEU.");
+        compPts++;
+      }
+      System.out.println("Jogador: " + jogPts + " Pontos");
+      System.out.println("Computador: " + compPts + " Pontos");
+      if (jogPts == 5) {
+        System.out.println("Vitória do Jogador");
+      }
+      if (compPts == 5) {
+        System.out.println("Vitória do Computador");
+      }
     }
-    //lógica do computador
-    computador = (int) (Math.random() * 3) + 1;
     sc.close();
-    switch (computador) {
-      case 1:
-        System.out.println("Computador escolheu PEDRA");
-        break;
-      case 2:
-        System.out.println("Computador escolheu TESOURA");
-        break;
-      case 3:
-        System.out.println("Computador escolheu PAPEL");
-        break;
-    }
-    //Lógica para determinar o vencedor
-    if (jogador == computador) {
-      System.out.println("EMPATE");
-    } else if (
-      (jogador == 1 && computador == 2) ||
-      (jogador == 2 && computador == 3) ||
-      (jogador == 3 && computador == 1)
-    ) {
-      System.out.println("JOGADOR VENCEU.");
-    } else {
-      System.out.println("COMPUTADOR VENCEU.");
-    }
   }
 }
